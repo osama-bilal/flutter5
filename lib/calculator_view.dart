d388843211264e5382ab367d03a1dc35dc39c505
osama-bilal <obilal076@gmail.com>
2023-09-22 20:51:39 +0300

    flutter HM 5

---
+++ lib/calculator_view.dart
@@ -0,0 +1,262 @@
+import 'package:flutter/material.dart';
+import 'package:simple_calculator/clacButton.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+class Calculator_view extends StatefulWidget {
+  const Calculator_view({Key? key}) : super(key: key);
+
+  @override
+  State<Calculator_view> createState() => _Calculator_viewState();
+}
+
+class _Calculator_viewState extends State<Calculator_view> {
+  String equation = "0";
+  String result = "0";
+  String expression = "";
+  double equationFontSize = 38.0;
+  double resultFontSize = 48.0;
+  buttonPressed(String buttonText) {
+    String doseContainDecimal(dynamic result) {
+      if (result.toString().contains('.')) {
+        List<String> splitDecimal = result.toString().split('.');
+        if (!(int.parse(splitDecimal[1]) > 0)) {
+          return result = splitDecimal[0].toString();
+        } else {
+          result = result.toString().split('.');
+        }
+      }
+      return result;
+    }
+
+    setState(() {
+      if (buttonText == "AC") {
+        equation = "0";
+        result = "0";
+      } else if (buttonText == "⌫") {
+        equation = equation.substring(0, equation.length - 1);
+        if (equation == "") {
+          equation = "0";
+        }
+      } else if (buttonText == "+/-") {
+        if (equation[0] != "-") {
+          equation = '-$equation';
+        } else {
+          equation = equation.substring(1);
+        }
+      } else if (buttonText == "=") {
+        if ((equation.endsWith(".") ||
+            equation.endsWith("+") ||
+            equation.endsWith("-") ||
+            equation.endsWith("÷") ||
+            equation.endsWith("⁒"))) {
+          equation = equation.substring(0, equation.length - 1);
+          buttonPressed("=");
+        }
+        expression = equation;
+        expression = expression.replaceAll('x', '*');
+        expression = expression.replaceAll('÷', '/');
+        expression = expression.replaceAll('⁒', '%');
+
+        try {
+          Parser p = Parser();
+          Expression exp = p.parse(expression);
+
+          ContextModel cm = ContextModel();
+          result = '${exp.evaluate(EvaluationType.REAL, cm)}';
+          if (expression.contains('%')) {
+            result = doseContainDecimal(result);
+          }
+        } catch (e) {
+          result = "Error";
+        }
+      } else if (buttonText == ".") {
+          equation += '.';
+      } else if ((buttonText == "-") ||
+          (buttonText == "+") ||
+          (buttonText == "⁒") ||
+          (buttonText == "x") ||
+          (buttonText == "÷")) {
+        if (equation.endsWith(".")) {
+          equation = equation.substring(0, equation.length - 1);
+        }
+        equation += buttonText;
+      } else {
+        if ((equation == "0") || (equation == "0.0")) {
+          equation = buttonText;
+        } else {
+          equation += buttonText;
+        }
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var row = Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text(
+            equation,
+            style: const TextStyle(
+              fontSize: 40,
+              color: Colors.white38,
+            ),
+          ),
+        ),
+        IconButton(
+          icon: const Icon(
+            Icons.backspace_outlined,
+            color: Colors.orange,
+            size: 30,
+          ),
+          onPressed: () => buttonPressed("⌫"),
+        ),
+        const SizedBox(
+          width: 20,
+        )
+      ],
+    );
+    return Scaffold(
+      backgroundColor: Colors.black54,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.black54,
+        leading: const Icon(
+          Icons.settings,
+          color: Colors.orange,
+        ),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(top: 18.0),
+            child: Text(
+              'DEG',
+              style: TextStyle(color: Colors.white38),
+            ),
+          ),
+          SizedBox(
+            width: 20,
+          )
+        ],
+      ),
+      body: SafeArea(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Align(
+            alignment: Alignment.bottomRight,
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                          result,
+                          textAlign: TextAlign.left,
+                          style: const TextStyle(
+                              color: Colors.white, fontSize: 80),
+                        ),
+                      ),
+                      const Icon(
+                        Icons.equalizer,
+                        color: Colors.orange,
+                        size: 30,
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      )
+                    ],
+                  ),
+                  row
+                ],
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              calcButton('AC', Colors.white10, () => buttonPressed('AC')),
+              calcButton('⁒', Colors.white10, () => buttonPressed('⁒')),
+              calcButton('÷', Colors.white10, () => buttonPressed('÷')),
+              calcButton('x', Colors.white10, () => buttonPressed('x')),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              calcButton('7', Colors.white24, () => buttonPressed('7')),
+              calcButton('8', Colors.white24, () => buttonPressed('8')),
+              calcButton('9', Colors.white24, () => buttonPressed('9')),
+              calcButton('-', Colors.white10, () => buttonPressed('-')),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              calcButton('4', Colors.white24, () => buttonPressed('4')),
+              calcButton('5', Colors.white24, () => buttonPressed('5')),
+              calcButton('6', Colors.white24, () => buttonPressed('6')),
+              calcButton('+', Colors.white10, () => buttonPressed('+')),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      calcButton('1', Colors.white24, () => buttonPressed('1')),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.04,
+                      ),
+                      calcButton('2', Colors.white24, () => buttonPressed('2')),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.04,
+                      ),
+                      calcButton('3', Colors.white24, () => buttonPressed('3')),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      calcButton(
+                          '+/-', Colors.white24, () => buttonPressed('+/-')),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.04,
+                      ),
+                      calcButton('0', Colors.white24, () => buttonPressed('0')),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.04,
+                      ),
+                      calcButton('.', Colors.white24, () => buttonPressed('.')),
+                    ],
+                  ),
+                ],
+              ),
+              calcButton('=', Colors.orange, () => buttonPressed('=')),
+            ],
+          )
+        ],
+      )),
+    );
+  }
+}
